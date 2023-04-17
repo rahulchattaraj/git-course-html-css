0e1c8d5df2b193e41df808cbc5082c09b0cd1f76
rahulchattaraj <26rahulchattaraj@gmail.com>
2023-04-17 16:52:10 +0530

    Revert "Finalized header menu"
    This reverts commit 4bd58ff8e7615d498e723ee23054f953595ba5b9.

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-document.querySelector("#open-nav-menu").addEventListener("click",() => {
-    document.querySelector("header nav .wrapper").classList.add("nav-open")
-});
-
-document.querySelector("#close-nav-menu").addEventListener("click",function(){
-    document.querySelector("header nav .wrapper").classList.remove("nav-open")
-})
+console.log("Welcome")
